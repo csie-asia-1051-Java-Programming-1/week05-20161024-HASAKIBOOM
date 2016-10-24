242bd2fb8916dba4a1611f8150b1acd3c5b7176b
HASAKIBOOM <105021063@live.asia.edu.tw>
2016-10-24 16:00:09 +0800

    交作業

--- src/ex/ex06.java
+++ src/ex/ex06.java
@@ -2,9 +2,9 @@
 /*
  * Topic: 某個陣列中含有所有學生的學號，但並沒有按照大小次序排列。請寫一程式依學號大小排列並將結果輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author:105021063 彭明捷
  */
-
+import java.util.Scanner;
 public class ex06 {
 
 	public static void main(String[] args) {
